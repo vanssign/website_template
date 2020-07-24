a27e294fc040a69b587973d6882c14d81cf6a8f9
vanssign <thindvansh1999@gmail.com>
2020-07-25 00:59:31 +0530

    treatment.html completed about contact page

---
+++ js/bootstap-js-config.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    //Tool Tip options
+    $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+//Carousel options
+$('.carousel').carousel({
+    interval: 5000
+  })
+});
